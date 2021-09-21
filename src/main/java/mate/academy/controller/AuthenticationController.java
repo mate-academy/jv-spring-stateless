1568b8c2d3f96ebaed843523253a20bd718b35d9
olekskov <oleksandr@kovalenko.cc>
2021-09-21 12:38:25 +0000

    JV-Spring-Stateless
    Initial solution:
    - JwtTokenFilter, Provider, Configurer implemented
    - SecurityConfig adopted
    - /login endpoint added
    - InvalidJwtAuthenticationException created/used
    
    N.B.
    - Some forked files contained code checkstyle errors so changed only due to this fact

--- src/main/java/mate/academy/controller/AuthenticationController.java
+++ src/main/java/mate/academy/controller/AuthenticationController.java
@@ -1,30 +1,46 @@
 package mate.academy.controller;
 
+import java.util.Map;
+import java.util.stream.Collectors;
 import javax.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import mate.academy.exception.AuthenticationException;
 import mate.academy.model.User;
+import mate.academy.model.dto.UserLoginDto;
 import mate.academy.model.dto.UserRegistrationDto;
 import mate.academy.model.dto.UserResponseDto;
 import mate.academy.security.AuthenticationService;
+import mate.academy.security.jwt.JwtTokenProvider;
 import mate.academy.service.mapper.UserMapper;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequiredArgsConstructor
 public class AuthenticationController {
     private final AuthenticationService authenticationService;
     private final UserMapper userMapper;
-
-    public AuthenticationController(AuthenticationService authenticationService,
-                                    UserMapper userMapper) {
-        this.authenticationService = authenticationService;
-        this.userMapper = userMapper;
-    }
+    private final JwtTokenProvider jwtTokenProvider;
 
     @PostMapping("/register")
     public UserResponseDto register(@RequestBody @Valid UserRegistrationDto userRequestDto) {
         User user = authenticationService.register(userRequestDto.getEmail(),
                 userRequestDto.getPassword());
         return userMapper.mapToDto(user);
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<Object> login(@RequestBody @Valid UserLoginDto userLoginDto)
+            throws AuthenticationException {
+        User user = authenticationService.login(userLoginDto.getLogin(),
+                userLoginDto.getPassword());
+        String token = jwtTokenProvider.createToken(user.getEmail(),
+                user.getRoles().stream()
+                .map(r -> r.getRoleName().name())
+                .collect(Collectors.toList()));
+        return new ResponseEntity<>(Map.of("token", token), HttpStatus.OK);
     }
 }
