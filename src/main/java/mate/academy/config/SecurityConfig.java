5988c092bce7d678d024b609301d00b22162691a
LinkClink <shekaqwe@gmail.com>
2022-04-09 17:44:54 +0300

    Update SecurityConfig.java

--- src/main/java/mate/academy/config/SecurityConfig.java
+++ src/main/java/mate/academy/config/SecurityConfig.java
@@ -1,26 +1,60 @@
 package mate.academy.config;
 
+import mate.academy.security.jwt.JwtConfigurer;
+import mate.academy.security.jwt.JwtTokenProvider;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
     private final UserDetailsService userDetailsService;
     private final PasswordEncoder passwordEncoder;
-
+    private final JwtTokenProvider jwtTokenProvider;
 
     public SecurityConfig(UserDetailsService userDetailsService,
-                          PasswordEncoder passwordEncoder) {
+                          PasswordEncoder passwordEncoder,
+                          JwtTokenProvider jwtTokenProvider) {
         this.userDetailsService = userDetailsService;
         this.passwordEncoder = passwordEncoder;
+        this.jwtTokenProvider = jwtTokenProvider;
     }
 
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
         auth.userDetailsService(userDetailsService)
                 .passwordEncoder(passwordEncoder);
+    }
+
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .httpBasic().disable()
+                .csrf().disable()
+                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and()
+                .authorizeRequests()
+//                .antMatchers(HttpMethod.POST, "/cinema-halls/**").hasRole("ADMIN")
+//                .antMatchers(HttpMethod.GET, "/cinema-halls/**").permitAll()
+//                .antMatchers(HttpMethod.POST, "/movies/**").hasRole("ADMIN")
+//                .antMatchers(HttpMethod.GET, "/movies/**").permitAll()
+//                .antMatchers("/movie-sessions/available").permitAll()
+//                .antMatchers("/movie-sessions/**").hasRole("ADMIN")
+//                .antMatchers("/orders/**").hasRole("USER")
+//                .antMatchers("/shopping-carts/**").hasRole("USER")
+//                .antMatchers(HttpMethod.GET, "/users/**").hasRole("ADMIN")
+                .antMatchers(HttpMethod.DELETE, "/products/*", "/users/*").hasRole("ADMIN")
+                .antMatchers("/register", "/login", "/inject").permitAll()
+                .anyRequest()
+                .authenticated()
+                .and()
+                .apply(new JwtConfigurer(jwtTokenProvider))
+                .and()
+                .headers().frameOptions().disable();
     }
 }
