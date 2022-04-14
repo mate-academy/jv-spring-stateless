7d7ec7b93ce00032196f2959aa5f1a7c41112a12
Andriy_Poluhovic <chimbar@ukr.net>
2022-04-14 20:48:49 +0300

    Was added token to project

---
+++ src/main/java/mate/academy/security/jwt/JwtTokenConfigurer.java
@@ -0,0 +1,23 @@
+package mate.academy.security.jwt;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.config.annotation.SecurityConfigurerAdapter;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.DefaultSecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+public class JwtTokenConfigurer extends SecurityConfigurerAdapter<DefaultSecurityFilterChain,
+        HttpSecurity> {
+    private final JwtTokenProvider jwtTokenProvider;
+
+    @Autowired
+    public JwtTokenConfigurer(JwtTokenProvider jwtTokenProvider) {
+        this.jwtTokenProvider = jwtTokenProvider;
+    }
+
+    @Override
+    public void configure(HttpSecurity builder) throws Exception {
+        JwtTokenFilter filter = new JwtTokenFilter(jwtTokenProvider);
+        builder.addFilterBefore(filter, UsernamePasswordAuthenticationFilter.class);
+    }
+}
