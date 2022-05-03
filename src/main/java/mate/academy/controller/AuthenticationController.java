54b7d2e7f6c02023e8e6439f89d7a2b833a1bbc1
anyutamysko <a_misko@ukr.net>
2022-05-03 19:43:34 +0300

    fixed checkstyle

--- src/main/java/mate/academy/controller/AuthenticationController.java
+++ src/main/java/mate/academy/controller/AuthenticationController.java
@@ -1,7 +1,6 @@
 package mate.academy.controller;
 
 import java.util.List;
-import java.util.Map;
 import java.util.stream.Collectors;
 import javax.validation.Valid;
 import mate.academy.exception.AuthenticationException;
