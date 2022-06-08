75ba8cea3a444a5f71eb1bd45c04cb85f7d89b65
Maksym Pokras <mxpokras@gmail.com>
2022-06-08 10:52:40 +0200

    /login endpoint implemented

--- src/main/java/mate/academy/security/jwt/JwtTokenProvider.java
+++ src/main/java/mate/academy/security/jwt/JwtTokenProvider.java
@@ -1,16 +1,16 @@
 package mate.academy.security.jwt;
 
-import java.util.Base64;
-import java.util.Date;
-import java.util.List;
-import javax.annotation.PostConstruct;
-import javax.servlet.http.HttpServletRequest;
-
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jws;
 import io.jsonwebtoken.JwtException;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
+import java.util.Base64;
+import java.util.Date;
+import java.util.List;
+import javax.annotation.PostConstruct;
+import javax.servlet.http.HttpServletRequest;
+import mate.academy.exception.InvalidJwtAuthenticationException;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
@@ -72,7 +72,7 @@ public class JwtTokenProvider {
             Jws<Claims> claims = Jwts.parser().setSigningKey(secretKey).parseClaimsJws(token);
             return !claims.getBody().getExpiration().before(new Date());
         } catch (JwtException | IllegalArgumentException e) {
-            throw new RuntimeException("Expired or invalid JWT token", e);
+            throw new InvalidJwtAuthenticationException("Expired or invalid JWT token", e);
         }
     }
 }
