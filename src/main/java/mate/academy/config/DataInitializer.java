4c05c008f2a7e2e4c588ecdb95038d1f98fa4602
anton-karpov <antonkarpov547@gmail.com>
2022-07-18 20:31:26 +0200

    Stateless application configured, JWT Filter, Provider and Configurer implemented, custom Exception created

---
+++ src/main/java/mate/academy/config/DataInitializer.java
@@ -0,0 +1,35 @@
+package mate.academy.config;
+
+import java.util.Set;
+import javax.annotation.PostConstruct;
+import mate.academy.model.Role;
+import mate.academy.model.User;
+import mate.academy.service.RoleService;
+import mate.academy.service.UserService;
+import org.springframework.stereotype.Component;
+
+@Component
+public class DataInitializer {
+    private final RoleService roleService;
+    private final UserService userService;
+
+    public DataInitializer(RoleService roleService, UserService userService) {
+        this.roleService = roleService;
+        this.userService = userService;
+    }
+
+    @PostConstruct
+    public void inject() {
+        Role adminRole = new Role();
+        adminRole.setRoleName(Role.RoleName.ADMIN);
+        roleService.save(adminRole);
+        Role userRole = new Role();
+        userRole.setRoleName(Role.RoleName.USER);
+        roleService.save(userRole);
+        User admin = new User();
+        admin.setEmail("admin@i.ua");
+        admin.setPassword("admin123");
+        admin.setRoles(Set.of(adminRole));
+        userService.save(admin);
+    }
+}
