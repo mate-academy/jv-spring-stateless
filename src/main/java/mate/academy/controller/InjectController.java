1186b08e35a1d9ece7226f3288f00819f86555bf
Igor <ZIM20093@gmail.com>
2022-08-01 02:05:35 +0300

    first commit

--- src/main/java/mate/academy/controller/InjectController.java
+++ src/main/java/mate/academy/controller/InjectController.java
@@ -41,13 +41,13 @@ public class InjectController {
 
         // save users
         User bob = new User();
-        bob.setEmail("bob");
+        bob.setEmail("bob@com.ua");
         bob.setPassword("1234");
         bob.setRoles(new HashSet<>(roles));
         userService.save(bob);
 
         User alice = new User();
-        alice.setEmail("alice");
+        alice.setEmail("alice@com.ua");
         alice.setPassword("1234");
         alice.setRoles(Set.of(roles.get(0)));
         userService.save(alice);
