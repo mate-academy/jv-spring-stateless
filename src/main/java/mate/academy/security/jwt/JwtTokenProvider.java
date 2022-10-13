8760693eb0fc590d3c58f5e152f851344cfb6875
Slava Verbanov <v.verbanov@gmail.com>
2022-10-13 17:47:07 +0200

    implement JwtTokenFilter, JwtConfigurer

--- src/main/java/mate/academy/security/jwt/JwtTokenProvider.java
+++ src/main/java/mate/academy/security/jwt/JwtTokenProvider.java
@@ -5,18 +5,17 @@ import io.jsonwebtoken.Jws;
 import io.jsonwebtoken.JwtException;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
+import javax.annotation.PostConstruct;
+import javax.servlet.http.HttpServletRequest;
+import java.util.Base64;
+import java.util.Date;
+import java.util.List;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.stereotype.Component;
-
-import javax.annotation.PostConstruct;
-import javax.servlet.http.HttpServletRequest;
-import java.util.Base64;
-import java.util.Date;
-import java.util.List;
 
 @Component
 public class JwtTokenProvider {
