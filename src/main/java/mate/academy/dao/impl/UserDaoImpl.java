fe584f8239145e87f7572a99d7b7b8fcc33e5ed9
Max Lutovinov <90464472+maxlutovinov@users.noreply.github.com>
2022-11-01 04:03:45 +0200

    Implement the login endpoint in AuthenticationController

--- src/main/java/mate/academy/dao/impl/UserDaoImpl.java
+++ src/main/java/mate/academy/dao/impl/UserDaoImpl.java
@@ -30,7 +30,7 @@ public class UserDaoImpl extends AbstractDao<User, Long> implements UserDao {
     public List<User> findAll() {
         try (Session session = sessionFactory.openSession()) {
             return session.createQuery(
-                    "from User u join fetch u.roles", User.class)
+                    "select distinct u from User u join fetch u.roles", User.class)
                 .getResultList();
         }
     }
