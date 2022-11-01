c0fda0dc1d0abcc8363ec0e782679eccd43d5474
Max Lutovinov <90464472+maxlutovinov@users.noreply.github.com>
2022-11-02 00:11:51 +0200

    Remove try/catch block from method in JwtTokenFilter

--- src/main/java/mate/academy/security/jwt/JwtTokenFilter.java
+++ src/main/java/mate/academy/security/jwt/JwtTokenFilter.java
@@ -5,7 +5,6 @@ import javax.servlet.FilterChain;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import mate.academy.exception.InvalidJwtAuthenticationException;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.filter.OncePerRequestFilter;
@@ -21,15 +20,9 @@ public class JwtTokenFilter extends OncePerRequestFilter {
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response,
                                     FilterChain filterChain) throws ServletException, IOException {
         String token = jwtTokenProvider.resolveToken(request);
-        try {
-            if (token != null && jwtTokenProvider.validateToken(token)) {
-                Authentication authentication = jwtTokenProvider.getAuthentication(token);
-                SecurityContextHolder.getContext().setAuthentication(authentication);
-            }
-        } catch (InvalidJwtAuthenticationException ex) {
-            SecurityContextHolder.clearContext();
-            response.sendError(ex.getHttpStatus().value(), ex.getMessage());
-            return;
+        if (token != null && jwtTokenProvider.validateToken(token)) {
+            Authentication authentication = jwtTokenProvider.getAuthentication(token);
+            SecurityContextHolder.getContext().setAuthentication(authentication);
         }
         filterChain.doFilter(request, response);
     }
