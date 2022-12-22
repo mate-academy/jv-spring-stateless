e6fa7af7cbd101e08b3366fb1096d1482bd43feb
galenkoigor <galenkoigor@gmail.com>
2022-12-22 23:44:07 +0100

    Configured SecurityConfig class

---
+++ src/main/java/mate/academy/security/jwt/JwtConfigurer.java
@@ -0,0 +1,14 @@
+package mate.academy.security.jwt;
+
+import org.springframework.security.config.annotation.SecurityConfigurerAdapter;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.DefaultSecurityFilterChain;
+
+public class JwtConfigurer extends
+        SecurityConfigurerAdapter<DefaultSecurityFilterChain, HttpSecurity> {
+    private final JwtTokenProvider jwtTokenProvider;
+
+    public JwtConfigurer(JwtTokenProvider jwtTokenProvider) {
+        this.jwtTokenProvider = jwtTokenProvider;
+    }
+}
---
+++ src/main/java/mate/academy/security/jwt/JwtTokenProvider.java
@@ -0,0 +1,7 @@
+package mate.academy.security.jwt;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class JwtTokenProvider {
+}
