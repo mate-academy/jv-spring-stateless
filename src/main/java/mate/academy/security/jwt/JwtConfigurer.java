b2502fc9303f9cb1960015f0aee0eba7675a3a80
galenkoigor <galenkoigor@gmail.com>
2022-12-23 17:10:23 +0100

    Implemented JwtConfigurer class

--- src/main/java/mate/academy/security/jwt/JwtConfigurer.java
+++ src/main/java/mate/academy/security/jwt/JwtConfigurer.java
@@ -3,6 +3,7 @@ package mate.academy.security.jwt;
 import org.springframework.security.config.annotation.SecurityConfigurerAdapter;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.DefaultSecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 public class JwtConfigurer extends
         SecurityConfigurerAdapter<DefaultSecurityFilterChain, HttpSecurity> {
@@ -10,5 +11,11 @@ public class JwtConfigurer extends
 
     public JwtConfigurer(JwtTokenProvider jwtTokenProvider) {
         this.jwtTokenProvider = jwtTokenProvider;
+    }
+
+    @Override
+    public void configure(HttpSecurity builder) {
+        JwtTokenFilter customFilter = new JwtTokenFilter(jwtTokenProvider);
+        builder.addFilterBefore(customFilter, UsernamePasswordAuthenticationFilter.class);
     }
 }
