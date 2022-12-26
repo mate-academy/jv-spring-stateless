846ed0edded0e226bef719c8cde67a0d167b7c06
NSay0 <krvipa@gmail.com>
2022-12-26 19:55:44 +0200

    Minor correction

--- src/main/java/mate/academy/security/jwt/JwtConfigurer.java
+++ src/main/java/mate/academy/security/jwt/JwtConfigurer.java
@@ -14,7 +14,7 @@ public class JwtConfigurer extends SecurityConfigurerAdapter<DefaultSecurityFilt
     }
 
     @Override
-    public void configure(HttpSecurity builder) throws Exception {
+    public void configure(HttpSecurity builder) {
         builder.addFilterBefore(new JwtTokenFilter(jwtTokenProvider),
                 UsernamePasswordAuthenticationFilter.class);
     }
