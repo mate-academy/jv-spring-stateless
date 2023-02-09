6591821d30f9cecfa19440162274b0a5e9b6800b
AndriySichniy <sichniy.andriy@gmail.com>
2023-02-09 15:23:37 +0200

    create and impl JwtTokenConfigurer

--- src/main/java/mate/academy/exception/InvalidJwtAuthenticationException.java
+++ src/main/java/mate/academy/exception/InvalidJwtAuthenticationException.java
@@ -1,6 +1,7 @@
 package mate.academy.exception;
 
-public class InvalidJwtAuthenticationException extends AuthenticationException{
+public class InvalidJwtAuthenticationException
+        extends AuthenticationException {
     public InvalidJwtAuthenticationException(String message) {
         super(message);
     }
