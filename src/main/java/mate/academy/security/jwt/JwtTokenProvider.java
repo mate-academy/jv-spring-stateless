00ab152d355ec7fb25037222afa0c31b4d367c0d
Maxim Surzhik <kabaneri15@gmail.com>
2023-02-27 13:12:34 +0200

    add prsf

--- src/main/java/mate/academy/security/jwt/JwtTokenProvider.java
+++ src/main/java/mate/academy/security/jwt/JwtTokenProvider.java
@@ -20,6 +20,7 @@ import org.springframework.stereotype.Component;
 
 @Component
 public class JwtTokenProvider {
+    private static final Integer READ_FROM = 7;
     @Value("${security.jwt.token.secret-key}")
     private String secretKey;
     @Value("${security.jwt.token.expire-length}")
@@ -51,7 +52,7 @@ public class JwtTokenProvider {
     public String resolveToken(HttpServletRequest servletRequest) {
         String bearerToken = servletRequest.getHeader("Authorization");
         if (bearerToken != null && bearerToken.startsWith("Bearer ")) {
-            return bearerToken.substring(7);
+            return bearerToken.substring(READ_FROM);
         }
         return null;
     }
