b5d8c954b23f5aa24058e2d0d828376c88ed1ace
solomiiavoloshyn <solomiiavoloshyn@gmail.com>
2023-03-08 13:28:02 +0200

    fixes

--- src/main/java/mate/academy/security/AuthenticationServiceImpl.java
+++ src/main/java/mate/academy/security/AuthenticationServiceImpl.java
@@ -6,18 +6,21 @@ import mate.academy.exception.AuthenticationException;
 import mate.academy.model.User;
 import mate.academy.service.RoleService;
 import mate.academy.service.UserService;
-import org.springframework.security.crypto.bcrypt.BCrypt;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
 public class AuthenticationServiceImpl implements AuthenticationService {
     private final UserService userService;
     private final RoleService roleService;
+    private final PasswordEncoder passwordEncoder;
 
     public AuthenticationServiceImpl(UserService userService,
-                                     RoleService roleService) {
+                                     RoleService roleService,
+                                     PasswordEncoder passwordEncoder) {
         this.userService = userService;
         this.roleService = roleService;
+        this.passwordEncoder = passwordEncoder;
     }
 
     @Override
@@ -33,7 +36,8 @@ public class AuthenticationServiceImpl implements AuthenticationService {
     @Override
     public User login(String login, String password) throws AuthenticationException {
         Optional<User> user = userService.findByEmail(login);
-        if (user.isEmpty() || !BCrypt.checkpw(password, user.get().getPassword())) {
+        String encodedPassword = passwordEncoder.encode(password);
+        if (user.isEmpty() || !user.get().getPassword().equals(encodedPassword)) {
             throw new AuthenticationException("Incorrect username or password!!!");
         }
         return user.get();
