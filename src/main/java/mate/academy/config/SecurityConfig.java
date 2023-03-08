ff992d97a1adcdd1161b1f66e752145b22aef426
Дмитро <dima.smyk99.19@gmail.com>
2023-03-08 13:01:10 +0200

    add jwt token

--- src/main/java/mate/academy/config/SecurityConfig.java
+++ src/main/java/mate/academy/config/SecurityConfig.java
@@ -1,8 +1,14 @@
 package mate.academy.config;
 
+import mate.academy.jwt.JwtConfigure;
+import mate.academy.jwt.JwtTokenProvider;
+import mate.academy.model.Role;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
@@ -10,16 +16,38 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
     private final UserDetailsService userDetailsService;
     private final PasswordEncoder passwordEncoder;
+    private final JwtTokenProvider jwtTokenProvider;
 
     public SecurityConfig(UserDetailsService userDetailsService,
-                          PasswordEncoder passwordEncoder) {
+                          PasswordEncoder passwordEncoder,
+                          JwtTokenProvider jwtTokenProvider) {
         this.userDetailsService = userDetailsService;
         this.passwordEncoder = passwordEncoder;
+        this.jwtTokenProvider = jwtTokenProvider;
     }
 
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
         auth.userDetailsService(userDetailsService)
                 .passwordEncoder(passwordEncoder);
+    }
+
+    protected void configure(HttpSecurity httpSecurity) throws Exception {
+        httpSecurity.httpBasic()
+                .disable()
+                .csrf()
+                .disable()
+                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and()
+                .authorizeRequests()
+                .antMatchers("/register", "/login", "/inject").permitAll()
+                .antMatchers(HttpMethod.DELETE).hasRole(Role.RoleName.ADMIN.name())
+                .anyRequest()
+                .authenticated()
+                .and()
+                .apply(new JwtConfigure((jwtTokenProvider)))
+                .and()
+                .headers()
+                .frameOptions().disable();
     }
 }
