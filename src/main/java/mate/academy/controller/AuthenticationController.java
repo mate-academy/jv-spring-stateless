2f9b253da68ff3f292a083e678c4a7f8e5d6f06c
solomiiavoloshyn <solomiiavoloshyn@gmail.com>
2023-03-08 13:13:03 +0200

    commit

--- src/main/java/mate/academy/controller/AuthenticationController.java
+++ src/main/java/mate/academy/controller/AuthenticationController.java
@@ -1,22 +1,32 @@
 package mate.academy.controller;
 
+import java.util.Map;
+import java.util.stream.Collectors;
 import javax.validation.Valid;
+import mate.academy.exception.AuthenticationException;
 import mate.academy.model.User;
+import mate.academy.model.dto.UserLoginDto;
 import mate.academy.model.dto.UserRegistrationDto;
 import mate.academy.model.dto.UserResponseDto;
 import mate.academy.security.AuthenticationService;
+import mate.academy.security.jwt.JwtTokenProvider;
 import mate.academy.service.mapper.UserMapper;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class AuthenticationController {
+    private final JwtTokenProvider provider;
     private final AuthenticationService authenticationService;
     private final UserMapper userMapper;
 
-    public AuthenticationController(AuthenticationService authenticationService,
+    public AuthenticationController(JwtTokenProvider provider,
+                                    AuthenticationService authenticationService,
                                     UserMapper userMapper) {
+        this.provider = provider;
         this.authenticationService = authenticationService;
         this.userMapper = userMapper;
     }
@@ -26,5 +36,17 @@ public class AuthenticationController {
         User user = authenticationService.register(userRequestDto.getEmail(),
                 userRequestDto.getPassword());
         return userMapper.mapToDto(user);
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<Object> login(@RequestBody @Valid UserLoginDto userLoginDto)
+            throws AuthenticationException {
+        User user = authenticationService.login(userLoginDto.getLogin(),
+                userLoginDto.getPassword());
+        String token = provider.createToken(user.getEmail(), user.getRoles()
+                .stream()
+                .map(r -> r.getRoleName().name())
+                .collect(Collectors.toList()));
+        return new ResponseEntity<>(Map.of("token", token), HttpStatus.OK);
     }
 }
