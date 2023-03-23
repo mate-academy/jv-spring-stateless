5d7c6c377e398153acf5b6647b572eb17cd0bc70
Oleksandr Kravchenko <oleksandr.kravchenko@spirent.com>
2023-03-24 00:11:51 +0200

    feat: jv-spring-stateless #comment was added using JWT token to the spring

---
+++ src/main/java/mate/academy/exception/InvalidJwtAuthenticationException.java
@@ -0,0 +1,12 @@
+package mate.academy.exception;
+
+public class InvalidJwtAuthenticationException extends RuntimeException {
+
+    public InvalidJwtAuthenticationException(String message) {
+        super(message);
+    }
+
+    public InvalidJwtAuthenticationException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
