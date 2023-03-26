685f40ee95d7dae4a8664f02239c1979bee0ed9e
Nazarii Kharyshyn <nazariykharyshyn@gmail.com>
2023-03-26 02:20:18 +0200

    added fixes: -removed redundant throwing exception in method

--- src/main/java/mate/academy/security/jwt/JwtConfigurer.java
+++ src/main/java/mate/academy/security/jwt/JwtConfigurer.java
@@ -14,7 +14,7 @@ public class JwtConfigurer extends SecurityConfigurerAdapter<DefaultSecurityFilt
     }
 
     @Override
-    public void configure(HttpSecurity builder) throws Exception {
+    public void configure(HttpSecurity builder) {
         JwtTokenFilter jwtTokenFilter = new JwtTokenFilter(jwtTokenProvider);
         builder.addFilterBefore(jwtTokenFilter, UsernamePasswordAuthenticationFilter.class);
     }
