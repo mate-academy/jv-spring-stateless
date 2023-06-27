662ab9b1ce96b75d59490cc7746aa413da04d595
Skrudj <ssinilov@gmail.com>
2023-06-27 15:32:55 +0300

    implemented /login endpoint and JWT classes

--- src/main/java/mate/academy/exception/InvalidJwtAuthenticationException.java
+++ src/main/java/mate/academy/exception/InvalidJwtAuthenticationException.java
@@ -1,7 +1,7 @@
 package mate.academy.exception;
 
-public class InvalidJwtAuthenticationException extends Exception {
-    public InvalidJwtAuthenticationException(String message) {
-        super(message);
+public class InvalidJwtAuthenticationException extends RuntimeException {
+    public InvalidJwtAuthenticationException(String message, Throwable cause) {
+        super(message, cause);
     }
 }
