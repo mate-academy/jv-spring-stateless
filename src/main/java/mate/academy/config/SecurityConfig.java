ac94a18631b293cd8c707bcf2e8d183af156c8d8
Alona Klishchenko <alyonaklischenko@gmail.com>
2023-07-21 22:35:08 +0300

    Added methods names in config file

--- src/main/java/mate/academy/config/SecurityConfig.java
+++ src/main/java/mate/academy/config/SecurityConfig.java
@@ -2,6 +2,7 @@ package mate.academy.config;
 
 import mate.academy.security.jwt.JwtConfigurer;
 import mate.academy.security.jwt.JwtTokenProvider;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -37,8 +38,8 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
                 .and()
                 .authorizeRequests()
-                .antMatchers("/register", "/login", "/inject").hasAnyRole("ADMIN", "USER")
-                .antMatchers("/products/{id}", "/users/{id}").hasRole("ADMIN")
+                .antMatchers(HttpMethod.POST, "/register", "/login", "/inject").hasAnyRole("ADMIN", "USER")
+                .antMatchers(HttpMethod.DELETE,"/products/{id}", "/users/{id}").hasRole("ADMIN")
                 .anyRequest().authenticated()
                 .and()
                 .apply(new JwtConfigurer(jwtTokenProvider))
